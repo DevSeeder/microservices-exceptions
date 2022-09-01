323ca632aa13e87ce855e8a4baf045afbf05708e
Maick Speck <maicksantos05@hotmail.com>
2022-09-01 09:20:21 -0300

    0.3

--- src/exception/empty-prop.exception.ts
+++ src/exception/empty-prop.exception.ts
@@ -1,13 +1,13 @@
-import { Extensions } from '../extensions';
-import { HttpStatus } from '@nestjs/common';
-import { CustomErrorException } from './custom-error.exception';
+// import { Extensions } from '../extensions';
+// import { HttpStatus } from '@nestjs/common';
+// import { CustomErrorException } from './custom-error.exception';
 
-export class EmptyPropException extends CustomErrorException {
-  constructor(element: string) {
-    super(
-      `The property '${element.capitalize()}' cannot be empty`,
-      HttpStatus.NOT_ACCEPTABLE,
-      2,
-    );
-  }
-}
+// export class EmptyPropException extends CustomErrorException {
+//   constructor(element: string) {
+//     super(
+//       `The property '${element.capitalize()}' cannot be empty`,
+//       HttpStatus.NOT_ACCEPTABLE,
+//       2,
+//     );
+//   }
+// }
--- src/exception/index.ts
+++ src/exception/index.ts
@@ -1,7 +1,7 @@
 // export { NotFoundException } from './not-found.exception';
 export { CustomErrorException } from './custom-error.exception';
 export { EmptyDataException } from './empty-data.exception';
-export { EmptyPropException } from './empty-prop.exception';
+// export { EmptyPropException } from './empty-prop.exception';
 // export { InvalidDataException } from './invalid-data.exception';
 export { MissingSeedException } from './missing-seed.exception';
 export { MongoDBException } from './mongodb.exception';
