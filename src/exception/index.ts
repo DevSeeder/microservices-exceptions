8830f90e8cfd7b9d21bd259106a7d6e9141f22dd
Maick Speck <maicksantos05@hotmail.com>
2022-09-01 09:19:16 -0300

    fix common dependency

--- src/exception/index.ts
+++ src/exception/index.ts
@@ -1,8 +1,8 @@
-export { NotFoundException } from './not-found.exception';
+// export { NotFoundException } from './not-found.exception';
 export { CustomErrorException } from './custom-error.exception';
 export { EmptyDataException } from './empty-data.exception';
 export { EmptyPropException } from './empty-prop.exception';
-export { InvalidDataException } from './invalid-data.exception';
+// export { InvalidDataException } from './invalid-data.exception';
 export { MissingSeedException } from './missing-seed.exception';
 export { MongoDBException } from './mongodb.exception';
 export { ResolutionException } from './resolution.exception';
--- src/exception/not-found.exception.ts
+++ src/exception/not-found.exception.ts
@@ -1,13 +1,13 @@
-import { Extensions } from '../extensions';
-import { HttpStatus } from '@nestjs/common';
-import { CustomErrorException } from './custom-error.exception';
+// import { Extensions } from '../extensions';
+// import { HttpStatus } from '@nestjs/common';
+// import { CustomErrorException } from './custom-error.exception';
 
-export class NotFoundException extends CustomErrorException {
-  constructor(element: string) {
-    super(
-      `${element.capitalize()} not found`,
-      HttpStatus.NOT_FOUND,
-      HttpStatus.NOT_FOUND,
-    );
-  }
-}
+// export class NotFoundException extends CustomErrorException {
+//   constructor(element: string) {
+//     super(
+//       `${element.capitalize()} not found`,
+//       HttpStatus.NOT_FOUND,
+//       HttpStatus.NOT_FOUND,
+//     );
+//   }
+// }
