ace87b44c30a4a4228dfe7f3df1be5190afbf684
Maick Speck <maicksantos05@hotmail.com>
2022-09-26 18:05:47 -0300

    getStatus

--- src/exception/custom-error.exception.ts
+++ src/exception/custom-error.exception.ts
@@ -1,12 +1,18 @@
 import { HttpException } from '@nestjs/common';
 
 export class CustomErrorException extends HttpException {
+  private statusCode: number;
   constructor(
     public message: string,
     status: number,
-    public errCode: number = -1
+    public errCode: number = -1,
   ) {
     super(message, status);
+    this.statusCode = status;
+  }
+
+  getStatus(): number {
+    return this.statusCode;
   }
 
   get type(): string {
